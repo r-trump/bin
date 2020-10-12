29a5eb1cdab33152b3e88348c7cfa5f880f02af1
Jeffrey Bodin <jeffreybodin@outlook.com>
2020-10-11 23:56:42 -0500

    2020-10-11 - bin - "StringPlus.js" - changes

--- StringPlus.js
+++ StringPlus.js
@@ -1,7 +1,17 @@
 Node require()
 
 
-
+var stringAdd = (outPutVariableNames, inputStrings) => {
+  let varsName = outPutVariableNames;
+  let newString = inputStrings;
+  let nameVar = () => {
+    let var1 = varsName;
+    return var1
+  }
+  let varX = nameVar();
+  let stringsSum = varX += newString;
+  return stringsSum;
+}
 
 var armyForTrumpObj = '
   # **[Army for Trump](https://www.armyfortrump.com/)** \n
